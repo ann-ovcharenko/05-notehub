1a4fb51786e7ab917a573442ba48092c665511a8
Anna Ovcharenko <anyutka1426@gmail.com>
2025-11-24 13:04:15 +0200

    fix

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import type { NoteListItem } from '../../types/note';
-import { deleteNote } from '../../services/noteService';
-import css from './NoteList.module.css'; 
+import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Note } from "../../types/note";
+import { deleteNote } from "../../services/noteService";
+import css from "./NoteList.module.css";
 
 interface NoteListProps {
-  notes: NoteListItem[];
+  notes: Note[];
 }
 
 const NoteList: React.FC<NoteListProps> = ({ notes }) => {
   const queryClient = useQueryClient();
 
-  const deleteNoteMutation = useMutation({ 
+  const deleteNoteMutation = useMutation({
     mutationFn: deleteNote,
-    
+
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] });
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
     },
     onError: (error) => {
-        console.error('Помилка при видаленні нотатки:', error);
-        alert('Не вдалося видалити нотатку. Перевірте консоль.');
-    }
+      console.error("Помилка при видаленні нотатки:", error);
+      alert("Не вдалося видалити нотатку. Перевірте консоль.");
+    },
   });
 
   const handleDelete = (id: string) => {
@@ -28,21 +28,21 @@ const NoteList: React.FC<NoteListProps> = ({ notes }) => {
   };
 
   return (
-    <ul className={css.list}> 
+    <ul className={css.list}>
       {notes.map((note) => (
-        <li key={note.id} className={css.listItem}> 
-          <h2 className={css.title}>{note.title}</h2> 
+        <li key={note.id} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
-          
+
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
-            
+
             <button
               className={css.button}
               onClick={() => handleDelete(note.id)}
-              disabled={deleteNoteMutation.isPending} 
+              disabled={deleteNoteMutation.isPending}
             >
-              {deleteNoteMutation.isPending ? 'Deleting...' : 'Delete'}
+              {deleteNoteMutation.isPending ? "Deleting..." : "Delete"}
             </button>
           </div>
         </li>
